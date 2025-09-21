3bfa605e0fa396537494617121b6cddaf8343a65
sakshigawandecse <sakshigawandecse@gmail.com>
2025-09-21 18:16:47 +0530

    New

--- server/server.js
+++ server/server.js
@@ -16,7 +16,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// Middleware
+app.use(cors({
+  origin: 'https://mern-5iy1.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+app.use(express.json());
+
 app.use(express.json());
 
 // Routes
