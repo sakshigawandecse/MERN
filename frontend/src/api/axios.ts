c44ce1916652d1eabc22430d8b82432007766845
sakshigawandecse <sakshigawandecse@gmail.com>
2025-09-25 19:58:29 +0530

    Added new feature to website

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // You don’t need to import AxiosRequestConfig anymore
